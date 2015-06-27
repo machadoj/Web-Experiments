834d38127865d959ebcc2a6e402bd0e87d02e150
Joy Francesca Machado <machadoj@ccs.neu.edu>
2015-06-27 19:50:01 -0400

    First Commit

---
+++ angular/js/HelloWorldApp.js
@@ -0,0 +1,32 @@
+﻿var app = angular.module("HelloWorldApp", []);
+
+app.controller("HelloWorldController",
+    function ($scope) {
+        console.log("Hello World from Controller");
+        $scope.hello = "Hello from Angular JS";
+    });
+
+app.controller("SimpleJSObject",
+    function ($scope) {
+
+        var employee = {
+            first: "Alice",
+            last: "Wonderland"
+        };
+        $scope.employee = employee;
+    });
+
+app.controller("ArrayJSObject",
+    function ($scope) {
+
+        var employees = [{
+            first: "Alice",
+            last: "Wonderland"
+        },
+        {
+            first: "Bob",
+            last : "Marley"
+        }];
+
+        $scope.employees = employees;
+    });
